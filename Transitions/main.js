e8f40f36f64e72a7406dd6d7bc1710e28eef34b5
robison-brooks <32085271+robison-brooks@users.noreply.github.com>
2017-10-13 00:34:11 -0700

    movie api working. trailer loading

--- Transitions/main.js
+++ Transitions/main.js
@@ -1,66 +1,103 @@
 function onload() {
    var search = document.createElement("div");
+
    var sBar = document.createElement("input");
-   var submitBtn = document.createElement("button");
-   var poster = document.createElement("img")
-
-   search.classList.add('search');
-
    sBar.classList.add('sBar');
-   sBar.setAttribute("id", "movie");
+   sBar.setAttribute("id", "input");
 
-   submitBtn.classList.add('submitBtn');
-   submitBtn.setAttribute("id", "submit");
-   submitBtn.setAttribute("onclick", "submitBtn()");
-   submitBtn.value = "Search";
+   var res = document.createElement("div");
+   res.classList.add('results');
+   res.setAttribute("id", "results");
 
-   poster.classList.add("poster");
+   var poster = document.createElement("img")
    poster.setAttribute("id", "dispPoster");
 
+
+   var mInfo = document.createElement("div");
+   mInfo.classList.add("info");
+   mInfo.setAttribute("id", "mInfo");
+
    search.appendChild(sBar);
-   search.appendChild(poster);
-   search.appendChild(submitBtn);
+   res.appendChild(poster);
 
    document.getElementById("container").appendChild(search);
+   document.getElementById("container").appendChild(res);
+   document.getElementById("infoCont").appendChild(mInfo);
+
 
 }
 
 function submitBtn() {
-   // var movie =
-   alert(document.getElementById("movie").value);
-
-   //var students = document.getElementById("students");
-   // var xhr = new XMLHttpRequest();
-	// xhr.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=45b19b4b50f27078c87fd53b39383140&query=" + movie + "&callback=?", true);
-	// xhr.onreadystatechange = function () {
-	// 	if (xhr.readyState == 4 && xhr.status == 200) {
-	// 		var results = xhr.responseText;
-   //       var obj = JSON.parse(results);
-	// 		var poster = JSON.obj[0].poster_path;
-   //       var overview = JSON.obj[0].overview;
-   //       var releaseDate = JSON.obj[0].release_date;
-   //       var popRating = JSON.obj[0].popularity;
-   //       document.getElementById("dispPoster").setAttribute("src", "http://image.tmdb.org/t/p/w500/" + poster);
-   //
-	// 	}
-	// }
-	// xhr.send();
-
+   var input = document.getElementById("input").value;
 
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-         document.getElementById("dispPoster").innerHTML = this.responseText;
-         alert(this.responseText);
          var obj = JSON.parse(this.responseText);
-         alert(obj.results[0].title);
+         var poster = obj.results[0].poster_path;
+         var movieID = obj.results[0].id;
+         //alert(movieID);
+         document.getElementById("mInfo").setAttribute("mID", movieID);
+         document.getElementById("dispPoster").setAttribute("src", "http://image.tmdb.org/t/p/w342/" + poster);
+
       }
    };
-  xhttp.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=45b19b4b50f27078c87fd53b39383140&query=" + movie + "&callback=test", true);
-  xhttp.send();
+   xhttp.open("GET", "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=" + input + "&language=en-US&api_key=45b19b4b50f27078c87fd53b39383140", true);
+   xhttp.send();
 }
 
-function dispOverview() {
-   var info = document.createElement("div");
-   info.classList.add("appear")
+function display_overview() {
+   var input = document.getElementById("input").value;
+
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+
+      }
+   };
+   xhttp.open("GET", "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=" + input + "&language=en-US&api_key=45b19b4b50f27078c87fd53b39383140", true);
+   xhttp.send();
+}
+
+function display_cast() {
+
+}
+
+function video_check() {
+   var movieID = document.getElementById("mInfo").getAttribute("mID");
+
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         var obj = JSON.parse(this.responseText);
+         var key = obj.results[0].key;
+
+         var video = document.createElement("video");
+
+         video.classList.add("vid")
+         video.setAttribute("id", "vids");
+         video.setAttribute("width", "640");
+         video.setAttribute("height", "360");
+         video.setAttribute("type", "video/youtube");
+         video.setAttribute("preload", "none");
+         
+         var vSource = document.createElement("Source");
+         vSource.setAttribute("id", "vSrc");
+         vSource.setAttribute("src", "https://www.youtube.com/watch?v=" + key)
+         video.appendChild(vSource);
+         document.getElementById("mInfo").appendChild(video);
+
+         var player = new MediaElementPlayer('vids', {
+		   pluginPath: "https://www.youtube.com/watch?v=" + key,
+		   success: function(mediaElement, originalNode, instance) {
+			      mediaElement.load();
+		}
+	});
+
+
+
+      }
+   };
+   xhttp.open("GET", "http://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=45b19b4b50f27078c87fd53b39383140", true);
+   xhttp.send();
 }
