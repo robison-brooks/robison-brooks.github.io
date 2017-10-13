02215604def701b121c98cab62cdd696a53e470a
robison-brooks <32085271+robison-brooks@users.noreply.github.com>
2017-10-13 02:39:33 -0700

    Videos, overview, and poster working. Fix video instances

--- Transitions/main.js
+++ Transitions/main.js
@@ -1,24 +1,46 @@
 function onload() {
+   document.getElementById("reloadBtn").style.display = "initial";
+   document.getElementById("new").style.display = "none";
    var search = document.createElement("div");
 
    var sBar = document.createElement("input");
    sBar.classList.add('sBar');
    sBar.setAttribute("id", "input");
+   search.appendChild(sBar);
 
    var res = document.createElement("div");
    res.classList.add('results');
    res.setAttribute("id", "results");
 
+   var vidWrapper = document.createElement("div");
+   vidWrapper.setAttribute("id", "vidWrapper");
+   vidWrapper.style.display = "none";
+
+   var video = document.createElement("video");
+   video.classList.add("vid");
+   video.setAttribute("id", "vids");
+   video.setAttribute("width", "640");
+   video.setAttribute("height", "360");
+   video.setAttribute("type", "video/youtube");
+   video.setAttribute("autoplay", "autoplay");
+   vidWrapper.appendChild(video);
+
+   var overview = document.createElement("p");
+   overview.classList.add("overview");
+   overview.setAttribute("id", "overview");
+   overview.style.display = "none";
+
    var poster = document.createElement("img")
    poster.setAttribute("id", "dispPoster");
-
+   poster.style.display = "none";
 
    var mInfo = document.createElement("div");
    mInfo.classList.add("info");
    mInfo.setAttribute("id", "mInfo");
 
-   search.appendChild(sBar);
    res.appendChild(poster);
+   res.appendChild(vidWrapper);
+   res.appendChild(overview);
 
    document.getElementById("container").appendChild(search);
    document.getElementById("container").appendChild(res);
@@ -34,25 +56,56 @@ function submitBtn() {
    xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
          var obj = JSON.parse(this.responseText);
-         var poster = obj.results[0].poster_path;
+         var posterPath = obj.results[0].poster_path;
          var movieID = obj.results[0].id;
          //alert(movieID);
          document.getElementById("mInfo").setAttribute("mID", movieID);
-         document.getElementById("dispPoster").setAttribute("src", "http://image.tmdb.org/t/p/w342/" + poster);
 
+         var poster = document.getElementById("dispPoster");
+         poster.setAttribute("src", "http://image.tmdb.org/t/p/w342/" + posterPath);
+         poster.style.display = "initial";
+         document.getElementById("results").appendChild(poster);
       }
    };
    xhttp.open("GET", "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=" + input + "&language=en-US&api_key=45b19b4b50f27078c87fd53b39383140", true);
    xhttp.send();
+
+   document.getElementById("reloadBtn").style.display = "none";
+   document.getElementById("new").style.display = "initial";
+}
+
+function display_poster() {
+   if (document.getElementById("vidWrapper").style.display == "initial") {
+      document.getElementById("vidWrapper").style.display = "none";
+   }
+   if (document.getElementById("overview").style.display == "initial") {
+      document.getElementById("overview").style.display = "none";
+   }
+   document.getElementById("dispPoster").style.display = "initial";
 }
 
 function display_overview() {
+   document.getElementById("overview").style.display = "initial";
    var input = document.getElementById("input").value;
+
+   if (document.getElementById("dispPoster").style.display == "initial") {
+      document.getElementById("dispPoster").style.display = "none";
+   }
+   if (document.getElementById("vidWrapper").style.display == "initial") {
+      document.getElementById("vidWrapper").style.display = "none";
+   }
 
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+         var obj = JSON.parse(this.responseText);
+         var overviewTxt = obj.results[0].overview;
 
+         var overview = document.getElementById("overview");
+         overview.innerHTML = overviewTxt;
+         overview.style.display = "initial";
+
+         document.getElementById("results").appendChild(overview);
       }
    };
    xhttp.open("GET", "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=" + input + "&language=en-US&api_key=45b19b4b50f27078c87fd53b39383140", true);
@@ -65,6 +118,14 @@ function display_cast() {
 
 function video_check() {
    var movieID = document.getElementById("mInfo").getAttribute("mID");
+   document.getElementById("dispPoster").style.display = "none";
+
+   if (document.getElementById("dispPoster").style.display == "initial") {
+      document.getElementById("dispPoster").style.display = "none";
+   }
+   if (document.getElementById("overview").style.display == "initial") {
+      document.getElementById("overview").style.display = "none";
+   }
 
    var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
@@ -72,30 +133,24 @@ function video_check() {
          var obj = JSON.parse(this.responseText);
          var key = obj.results[0].key;
 
-         var video = document.createElement("video");
+         var vidWrapper = document.getElementById("vidWrapper");
+         vidWrapper.style.display = "initial";
 
-         video.classList.add("vid")
-         video.setAttribute("id", "vids");
-         video.setAttribute("width", "640");
-         video.setAttribute("height", "360");
-         video.setAttribute("type", "video/youtube");
-         video.setAttribute("preload", "none");
-         
+         var video = document.getElementById("vids");
          var vSource = document.createElement("Source");
          vSource.setAttribute("id", "vSrc");
          vSource.setAttribute("src", "https://www.youtube.com/watch?v=" + key)
          video.appendChild(vSource);
-         document.getElementById("mInfo").appendChild(video);
+
+         vidWrapper.appendChild(video);
+         document.getElementById("results").appendChild(vidWrapper);
 
          var player = new MediaElementPlayer('vids', {
-		   pluginPath: "https://www.youtube.com/watch?v=" + key,
-		   success: function(mediaElement, originalNode, instance) {
-			      mediaElement.load();
-		}
-	});
-
-
-
+            pluginPath: vSource.getAttribute("src"),
+            success: function(mediaElement, originalNode, instance) {
+               mediaElement.load();
+            }
+         });
       }
    };
    xhttp.open("GET", "http://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=45b19b4b50f27078c87fd53b39383140", true);
