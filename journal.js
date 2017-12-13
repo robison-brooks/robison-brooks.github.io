8813e4744e657edfa4497a2cad3f00a2a279bb00
robison-brooks <32085271+robison-brooks@users.noreply.github.com>
2017-12-13 01:11:24 -0800

    Journal upload

---
+++ journal.js
@@ -0,0 +1,6 @@
+function clickMe() {
+   document.getElementById("spacer").style.display = "none";
+   document.getElementById("beginning").style.display = "none";
+   document.getElementById("myBtn").style.display = "none";
+   document.getElementById("journal").style.opacity = "1";
+}
